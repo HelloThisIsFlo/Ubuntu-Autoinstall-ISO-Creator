945f4da547ba4ba4d47e3eb25168c7db25567cce
Florian Kempenich <Flori@nKempenich.com>
2022-12-31 18:23:51 +0000

    Write tool to encrypt password

---
+++ encrypt_password.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eo pipefail
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+cd "$DIR"
+
+# Install 'pwgen' cli tool
+sudo apt install pwgen >/dev/null 2>&1
+
+# Ask user to enter new password
+echo "Enter password to encrypt:"
+read -s PASSWORD
+
+# Generate a random salt using the pwgen utility
+salt=$(pwgen -s 8 1)
+# Generate a password hash using the salt and the SHA-512 algorithm
+password_hash=$(openssl passwd -6 -salt $salt "$PASSWORD")
+
+echo "This is the encrypted password"
+echo
+echo "    $password_hash"
+echo
+
+cd - >/dev/null
