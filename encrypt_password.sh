6f93c19764e344533812b36285f4ed17d4d2deed
Florian Kempenich <Flori@nKempenich.com>
2023-01-01 17:13:55 +0000

    Revert "Simplify encrypt password script"
    This reverts commit 0a0768e8c33538369d4f9cc68530a2a96da542a4.

--- encrypt_password.sh
+++ encrypt_password.sh
@@ -4,6 +4,21 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd "$DIR"
 
-python -c 'import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))'
+# Install 'pwgen' cli tool
+sudo apt install pwgen >/dev/null 2>&1
+
+# Ask user to enter new password
+echo "Enter password to encrypt:"
+read -s PASSWORD
+
+# Generate a random salt using the pwgen utility
+salt=$(pwgen -s 8 1)
+# Generate a password hash using the salt and the SHA-512 algorithm
+password_hash=$(openssl passwd -6 -salt $salt "$PASSWORD")
+
+echo "This is the encrypted password"
+echo
+echo "    $password_hash"
+echo
 
 cd - >/dev/null
