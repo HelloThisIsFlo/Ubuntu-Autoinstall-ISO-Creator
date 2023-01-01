0a0768e8c33538369d4f9cc68530a2a96da542a4
Florian Kempenich <Flori@nKempenich.com>
2023-01-01 16:22:05 +0000

    Simplify encrypt password script

--- encrypt_password.sh
+++ encrypt_password.sh
@@ -4,21 +4,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 cd "$DIR"
 
-# Install 'pwgen' cli tool
-sudo apt install pwgen >/dev/null 2>&1
-
-# Ask user to enter new password
-echo "Enter password to encrypt:"
-read -s PASSWORD
-
-# Generate a random salt using the pwgen utility
-salt=$(pwgen -s 8 1)
-# Generate a password hash using the salt and the SHA-512 algorithm
-password_hash=$(openssl passwd -6 -salt $salt "$PASSWORD")
-
-echo "This is the encrypted password"
-echo
-echo "    $password_hash"
-echo
+python -c 'import crypt,getpass; print(crypt.crypt(getpass.getpass(), crypt.mksalt(crypt.METHOD_SHA512)))'
 
 cd - >/dev/null
